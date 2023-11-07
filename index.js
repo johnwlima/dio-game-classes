a146259e2dfb316c7f272f63b17ef143d92adbe4
John Lima <jwesley1210@gmail.com>
2023-11-06 23:29:31 -0300

    Create Hero class

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
-//Objetivo:
-// Crie uma classe generica que represente um herói de uma aventura e que possua as seguintes propriedades:
-// - nome
-// - idade
-// - tipo (ex: guerreiro, mago, monge, ninja )
-
-let name;
-let age;
-let atackType;
-
-toAtack("ninja");
-
-// função atacar
-function toAtack(type) {
-
-    switch(type) {
-
-        case type = "mago":
-            atackType = "magia"
-            break
-        case type = "guerreiro":
-            atackType = "espada"
-            break
-        case type = "monge":
-            atackType = "artes marciais"
-            break
-        case type = "ninja":
-            atackType = "shuriken"
-            break
-        default:
-            atackType = "soco"
+class Hero {
+    // propriedades
+    constructor(name, age, type) {
+        this.name = name;
+        this.age = age;
+        this.type = type;
     }
 
-    return console.log(`o ${type} atacou usando ${atackType}`)
+    // métodos
+    toAttack() {
+        let attackType = ""; // Inicializa a variável attackType
+
+        switch (this.type) {
+            case "mago":
+                attackType = "magia";
+                break;
+            case "guerreiro":
+                attackType = "espada";
+                break;
+            case "monge":
+                attackType = "artes marciais";
+                break;
+            case "ninja":
+                attackType = "shuriken";
+                break;
+            default:
+                attackType = "soco";
+        }
+
+        console.log(`O ${this.type} ${this.name} atacou usando ${attackType}`);
+    }
 }
 
-
+let hero = new Hero("Alef", 23, "ninja");
+hero.toAttack();
